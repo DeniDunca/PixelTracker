44ccfde115557d86d8e235a9e844b2acc4b00a85
Denis Dunca <duncadenisa05@gmail.com>
2025-08-17 22:18:59 +0300

    working on modal

--- frontend/src/app/components/pixelModal.js
+++ frontend/src/app/components/pixelModal.js
@@ -2,14 +2,28 @@ import styles from "./pixelModal.module.css";
 import { usePixelStore } from "../hooks/PixelContext";
 
 export default function PixelModal() {
-
   const { showModal, toggleModal } = usePixelStore();
 
   return (
     <div className={styles.modalWrapper}>
       <div className={styles.modal}>
-        <span className={styles.close} onClick={toggleModal}>&times;</span>
-        <p>Some text in the Modal..</p>
+        <span className={styles.close} onClick={toggleModal}>
+          &times;
+        </span>
+        <div className={styles.modalHeader}>
+          <p>Workout pixel</p>
+          <label>17/08/25</label>
+        </div>
+        <div className={styles.modalBody}>
+          <div className={styles.pixel}/>
+          <div className={styles.colors}>
+            <select></select>
+          </div>
+        </div>
+        <div className={styles.modalFooter}>
+          <button>Save</button>
+          <button>Close</button>
+        </div>
       </div>
     </div>
   );
